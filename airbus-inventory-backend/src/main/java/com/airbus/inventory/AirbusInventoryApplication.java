a880c7cba6aaf9c5d41cacd3b4aaaedda93067a6
Rahul Juluru <rahuljuluru786@gmail.com>
2026-08-29 17:29:54 -0500

    Set up Spring Boot project with Maven, JDBC, and Security dependencies

---
+++ airbus-inventory-backend/src/main/java/com/airbus/inventory/AirbusInventoryApplication.java
@@ -0,0 +1,12 @@
+package com.airbus.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbusInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbusInventoryApplication.class, args);
+    }
+}
